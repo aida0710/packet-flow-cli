8a85283557a7c496da89d0783c4b6d00d3d16fb2
相田 優希 <51500566+aida0710@users.noreply.github.com>
2025-01-03 17:33:57 +0900

    時間の入力ができるように

--- src/config/error.rs
+++ src/config/error.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDateTime;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -10,4 +11,16 @@ pub enum ConfigError {
 
     #[error("環境変数の解析に失敗しました: {0}")]
     EnvVarParseError(String),
+
+    #[error("終了日時は開始日時より後である必要があります: {0} > {1}")]
+    InvalidDateOrder(NaiveDateTime, NaiveDateTime),
+
+    #[error("入力は14桁である必要がありますが、受け取った長さは {0} 桁です。")]
+    InvalidInputLength(String),
+
+    #[error("入出力エラー: {0}")]
+    IoError(String),
+
+    #[error("日時のパースエラー: {0}")]
+    ParseError(String),
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,5 +1,7 @@
 mod app_config;
 mod error;
+mod date_input;
 
 pub use app_config::AppConfig;
 pub use app_config::LoggerConfig;
+pub use date_input::DateTimeInput;
